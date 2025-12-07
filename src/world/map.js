c4df1419e8e8a1d6241be083bb0e51b9f79d96ed
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-12-07 04:27:10 +0300

    Update map.js

--- src/world/map.js
+++ src/world/map.js
@@ -1,13 +1,12 @@
-const THREE = window.THREE;
 import { addHouse } from "./buildings.js";
+const THREE = window.THREE;
 
 export async function loadWorld(scene) {
-  // أرض كبيرة
+  // الأرض
   const ground = new THREE.Mesh(
     new THREE.PlaneGeometry(500, 500),
     new THREE.MeshStandardMaterial({ color: 0x228b22 })
   );
-
   ground.rotation.x = -Math.PI / 2;
   scene.add(ground);
 
