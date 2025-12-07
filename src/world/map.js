dccebbd66b35dd0d231726ebc263311a65b6ce64
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-12-07 04:44:28 +0300

    Update map.js

--- src/world/map.js
+++ src/world/map.js
@@ -4,10 +4,10 @@ export async function loadWorld(scene) {
   // أرض واسعة
   const ground = new THREE.Mesh(
     new THREE.PlaneGeometry(500, 500),
-    new THREE.MeshStandardMaterial({ color: 0x145814 })
+    new THREE.MeshStandardMaterial({ color: 0x145814 }) // أخضر غامق
   );
   ground.rotation.x = -Math.PI / 2;
   scene.add(ground);
 
-  // لاحقاً: هنا سنضيف الجبال والأشجار باستخدام صورك
+  // لاحقاً سنضيف هنا الجبال والأشجار والصور اللي أرسلتها
 }
