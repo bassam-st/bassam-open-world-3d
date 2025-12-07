2acf0ce6d101ef9e4ab37eb8f5e38495749c42f1
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-12-07 04:38:10 +0300

    Update map.js

--- src/world/map.js
+++ src/world/map.js
@@ -1,15 +1,13 @@
-import { addHouse } from "./buildings.js";
 const THREE = window.THREE;
 
 export async function loadWorld(scene) {
-  // الأرض
+  // أرض واسعة
   const ground = new THREE.Mesh(
     new THREE.PlaneGeometry(500, 500),
-    new THREE.MeshStandardMaterial({ color: 0x228b22 })
+    new THREE.MeshStandardMaterial({ color: 0x145814 })
   );
   ground.rotation.x = -Math.PI / 2;
   scene.add(ground);
 
-  // إضافة البيت
-  addHouse(scene);
+  // لاحقاً: هنا سنضيف الجبال والأشجار باستخدام صورك
 }
