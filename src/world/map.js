1a779ca883beb2d8bc2500bf2460e6feb340621d
Bassam <78672415+bassam-st@users.noreply.github.com>
2025-12-07 04:20:33 +0300

    Update map.js

--- src/world/map.js
+++ src/world/map.js
@@ -1,15 +1,16 @@
 const THREE = window.THREE;
+import { addHouse } from "./buildings.js";
 
 export async function loadWorld(scene) {
-  // أرض كبيرة (يمكننا تغييرها لاحقاً حسب الصور)
-  const groundGeo = new THREE.PlaneGeometry(200, 200);
-  const groundMat = new THREE.MeshStandardMaterial({ color: 0x228b22 });
-  const ground = new THREE.Mesh(groundGeo, groundMat);
+  // أرض كبيرة
+  const ground = new THREE.Mesh(
+    new THREE.PlaneGeometry(500, 500),
+    new THREE.MeshStandardMaterial({ color: 0x228b22 })
+  );
 
   ground.rotation.x = -Math.PI / 2;
-  ground.receiveShadow = true;
-
   scene.add(ground);
 
-  // لاحقاً سنضيف المباني/الطرق هنا حسب الصور التي سترسلها
+  // إضافة البيت
+  addHouse(scene);
 }
